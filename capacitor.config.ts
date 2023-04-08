8d77b83402006882b200c19de8160d0647fda6a9
BytesCrafter <bytescrafter@gmail.com>
2023-04-08 15:20:16 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pusta.app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
