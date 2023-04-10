b8b980cd7177b4162dc3d2cd2374a78272e0ca11
BytesCrafter <bytescrafter@gmail.com>
2023-04-10 14:25:17 +0800

    Added a sample darkmode integration to variable scss.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, EnvironmentInjector, inject } from '@angular/core';
+import { Component, EnvironmentInjector, Inject, inject } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -12,5 +12,39 @@ import { CommonModule } from '@angular/common';
 export class AppComponent {
   public environmentInjector = inject(EnvironmentInjector);
 
-  constructor() {}
+  constructor(
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    // Use matchMedia to check the user preference
+    const prefersDark = window.matchMedia('(prefers-color-scheme: light)');
+
+    //check if system is on light.
+    const isLight: boolean = prefersDark.matches;
+    console.log("System theme is "+(isLight?'light':'dark'));
+
+    this.checkTheme();
+  }
+
+  checkTheme() {
+    const themeSet = localStorage.getItem('app-color-scheme');
+    const curTheme = themeSet ? themeSet : 'auto';
+
+    if(curTheme !== 'auto') {
+      this.changeTheme(curTheme=='dark'?true:false);
+    }
+  }
+
+  switchTheme(event: any) {
+    this.changeTheme(event.detail.checked);
+  }
+
+  changeTheme(toDark: boolean = false) {
+    //Change the theme state.
+    this.document.body.classList.remove(toDark?'light':'dark'); //change theme
+    this.document.body.classList.add(toDark?'dark':'light'); //change theme
+
+    //save the theme state
+    const isDark = this.document.body.classList.contains('dark');
+    localStorage.setItem('app-color-scheme', isDark?'dark':'light');
+  }
 }
