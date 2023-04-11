99a0b8202120ba492c2326f9d0b4fb79f9c13003
BytesCrafter <bytescrafter@gmail.com>
2023-04-11 18:51:47 +0800

    Changes the default app name.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'pusta.app',
+  appName: 'ionic.app',
   webDir: 'www',
   bundledWebRuntime: false
 };
