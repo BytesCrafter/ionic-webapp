b90f3b7189d150994e522dd8cad74d5d16d82296
BytesCrafter <bytescrafter@gmail.com>
2023-06-19 22:20:41 +0800

    Update the content of the onboarding page except for the images.

--- src/app/info/onboarding/onboarding.page.ts
+++ src/app/info/onboarding/onboarding.page.ts
@@ -18,18 +18,23 @@ export class OnboardingPage implements OnInit {
   public slideData: any[] = [
     {
       imageUrl: "assets/images/onboarding/slide-1.png",
-      title: "Welcome",
-      description: "The ionic app is a practical preview of the ionic framework in action, and a demonstration of proper code use."
+      title: "Ease of Use and Accessibility",
+      description: "Ensure that the app is user-friendly and accessible for individuals with different abilities. Consider accommodating alternative biometric methods or providing options for individuals who may have difficulty using certain modalities."
     },
     {
       imageUrl: "assets/images/onboarding/slide-2.png",
-      title: "What is Ionic?",
-      description: "An open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript."
+      title: "Security and Privacy",
+      description: "Biometric data is sensitive information, so it's crucial to prioritize security and privacy. Implement strong encryption and storage protocols to safeguard the biometric data and ensure compliance with relevant data protection regulations."
     },
     {
       imageUrl: "assets/images/onboarding/slide-3.png",
-      title: "What is Appflow?",
-      description: "A powerful set of services and features built on top of Ionic Framework that brings a totally new level of app development agility to mobile dev teams.",
+      title: "Data Analytics and Reporting",
+      description: "Implement features that allow managers or administrators to view and analyze the data collected by the app. This can provide insights into agent attendance, performance, and overall workforce management."
+    },
+    {
+      imageUrl: "assets/images/onboarding/slide-4.png",
+      title: "Continuous Improvements",
+      description: "Regularly update the app to address any bugs, security vulnerabilities, or usability issues. Seek feedback from users to identify areas for improvement and incorporate their suggestions into future updates.",
       action: {
         url: "/home",
         text: "Continue"
